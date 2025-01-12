77aa1f69328e09841f1c568f707b6fd34efe6090
P0 <143712083+DaRaP0@users.noreply.github.com>
2025-01-12 14:47:48 +0700

    Updated

--- sad/main.py
+++ sad/main.py
@@ -1,7 +1,7 @@
 from pygame import *
-from spriteClass import GameSprite
+from spriteClass import GameSprite, Player
 '''' variables '''
-bkg_color = (200,255,255)
+bkg_color = (200, 255, 255)
 win_width = 600
 win_height = 500
 
@@ -10,28 +10,33 @@ window.fill(bkg_color)
 
 clock = time.Clock()
 
-'''objects'''
-ball = GameSprite(player_image='sad/baller.jpg'
-                    player_x = 250,
-                    player_y = 250,
-                    width = 30,
-                    height = 50,
-                    speed = 2)
+''' objects '''
+ball = GameSprite(player_image='sad/baller.jpg',
+                   player_x=250, player_y=250, 
+                   width=50, heigth=50, speed=2)
 
-racket = GameSprite(player_image='sad/paddler.jpg'
-                    player_x = 250,
-                    player_y = 250,
-                    width = 30,
-                    height = 50,
-                    speed = 2)
+rocket_left = Player(player_image='sad/paddler.jpg',
+                   player_x=10, player_y=220, 
+                   width=50, heigth=150, speed=4)
 
+rocket_right = Player(player_image='sad/paddler.jpg',
+                   player_x=540, player_y=220, 
+                   width=50, heigth=150, speed=4)
 
-'''game loop'''
+''' game loop '''
 running = False
 while not running:
+
+    ball.reset(window)
+    rocket_left.reset(window)
+    rocket_left.update_p_left()
+
+    rocket_right.reset(window)
+    rocket_right.update_p_right()
+
     for e in event.get():
         if e.type == QUIT:
             running == True
 
     display.update()
-    clock.tick(60)
+    clock.tick(60) 
