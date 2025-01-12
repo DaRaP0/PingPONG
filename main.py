969b8fc5f6e7172844c0672460cb223d4c50428d
P0 <143712083+DaRaP0@users.noreply.github.com>
2025-01-12 14:04:33 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from pygame import *
+
+'''' variables '''
+bkg_color = (200,255,255)
+win_width = 600
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
+window.fill(bkg_color)
+
+clock = time.Clock()
+
+
+'''game loop'''
+running = False
+while not running:
+    for e in event.get():
+        if e.type == QUIT:
+            running == True
+
+    display.update()
+    clock.tick(60)
+
